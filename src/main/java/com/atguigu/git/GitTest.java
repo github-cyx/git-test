360036a5b693f57378504c4d95003ca2830b43fe
cyx <2332767474@qq.com>
2022-08-22 17:32:47 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author CYX
+ * @date 2022/8/22 17:27
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, git!");
+    }
+}
